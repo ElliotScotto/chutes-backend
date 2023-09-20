30370838265137121cee326a27c9dfe194c2c37e
Elliot Scotto <elliotscotto@macbook-pro-de-elliot.home>
2023-09-20 09:55:02 +0200

    secure

--- myproject/settings.py
+++ myproject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=5&dc6g6u7&260$w9biieiu#mmx^9@ut)g-ge=d62si&-_5ike'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -79,12 +79,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'scraps_database',
-        'USER': 'elliotscotto',
-        'PASSWORD': '2chutes&P@f',
-        'HOST': 'localhost',
-        'PORT': '5432',
+       'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
@@ -140,9 +140,9 @@ REST_FRAMEWORK = {
 
 CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
-    "http://192.168.1.38:8000",
-    "http://192.168.1.28:8000"
+     config('HOST_ONE'),
+    config('HOST_TWO'),
+    config('HOST_THREE'),
+    config('HOST_FOUR')
 ]
 ALLOWED_HOSTS = ['*']

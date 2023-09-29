94b9dcb1fdaf5b6ee9066f720dccf7fe6a89cb49
Elliot Scotto <elliotscotto@macbook-pro-de-elliot.home>
2023-09-29 08:45:35 +0200

    5photos ScrapSerializers

--- products/serializers.py
+++ products/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import UserCustom, Scrap
+from django.conf import settings
 
 class UserCustomSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,6 +11,33 @@ class ScrapSerializer(serializers.ModelSerializer):
     owner = serializers.PrimaryKeyRelatedField(queryset=UserCustom.objects.all())
     owner_detail = UserCustomSerializer(source='owner', read_only=True)
 
+    photo1_url = serializers.SerializerMethodField()
+    photo2_url = serializers.SerializerMethodField()
+    photo3_url = serializers.SerializerMethodField()
+    photo4_url = serializers.SerializerMethodField()
+    photo5_url = serializers.SerializerMethodField()
+
+    def get_photo1_url(self, obj):
+        if obj.photo1:
+            return settings.MEDIA_URL + str(obj.photo1)
+        return None
+    def get_photo2_url(self, obj):
+        if obj.photo2:
+            return settings.MEDIA_URL + str(obj.photo2)
+        return None
+    def get_photo3_url(self, obj):
+        if obj.photo3:
+            return settings.MEDIA_URL + str(obj.photo3)
+        return None
+    def get_photo4_url(self, obj):
+        if obj.photo4:
+            return settings.MEDIA_URL + str(obj.photo4)
+        return None
+    def get_photo5_url(self, obj):
+        if obj.photo5:
+            return settings.MEDIA_URL + str(obj.photo5)
+        return None
+    
     def validate_material(self, value):
         if len(value) > 2:
             raise serializers.ValidationError("Vous ne pouvez sélectionner que 2 matériaux au maximum.")
@@ -28,4 +56,4 @@ class ScrapSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Scrap
-        fields = ('id', 'owner', 'owner_detail', 'photo1', 'photo2', 'photo3', 'photo4', 'photo5', 'name', 'description', 'condition', 'price', 'weight', 'material', 'category', 'home_pickup', 'product_location', 'created_at')
+        fields = ('id', 'owner', 'owner_detail', 'photo1_url', 'photo2_url', 'photo3_url', 'photo4_url', 'photo5_url', 'name', 'description', 'condition', 'price', 'weight', 'material', 'category', 'home_pickup', 'product_location', 'created_at')
--- products/views.py
+++ products/views.py
@@ -1,8 +1,14 @@
+import logging
 from django.http import HttpResponse
 from rest_framework import generics,status
 from rest_framework.response import Response
+from rest_framework.parsers import MultiPartParser
 from .models import UserCustom,Scrap
 from .serializers import ScrapSerializer
+from django.core.files.uploadedfile import UploadedFile
+
+
+logger = logging.getLogger(__name__)
 
 def index(request):
     return HttpResponse("Hello, World!")
@@ -12,25 +18,36 @@ def home(request):
 class ScrapListCreate(generics.ListCreateAPIView):
     queryset = Scrap.objects.all().select_related('owner')
     serializer_class = ScrapSerializer
+    parser_classes = (MultiPartParser,)
 
     def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
+        logger.debug("Starting POST method")
 
+        serializer = self.get_serializer(data=request.data)
+        logger.debug("Serializer initialized with request data")
+        # Afficher le contenu de request.data
+        print("request.data:", request.data)
+    
+        # Afficher les fichiers inclus dans la requête
+        print("request.FILES:", request.FILES)
         if serializer.is_valid():
+            logger.debug("Serializer is valid")
+
             self.perform_create(serializer)
+            logger.debug("Object created with serializer")
+
             headers = self.get_success_headers(serializer.data)
             return Response({
                 "status": status.HTTP_201_CREATED,
                 "message": "Produit créé avec succès.",
-                "data": serializer.data
             }, status=status.HTTP_201_CREATED, headers=headers)
-
+        else:
+            logger.error("Serializer is not valid. Errors: %s", serializer.errors)
         # Si le serializer est invalide, renvoyez les erreurs
         return Response({
             "status": status.HTTP_400_BAD_REQUEST,
             "message": "Erreur lors de la création du produit.",
             "errors": serializer.errors,  # Voici les erreurs détaillées
-            "data": request.data
         }, status=status.HTTP_400_BAD_REQUEST)
 
     def perform_create(self, serializer):
@@ -38,4 +55,5 @@ class ScrapListCreate(generics.ListCreateAPIView):
             # Définir l'owner comme étant le premier utilisateur
             serializer.save(owner=UserCustom.objects.first())
         except Exception as e:
-            return Response({"status": "error", "message": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+            logger.error("Error in perform_create: %s", str(e))
+            raise e  # re-raise the exception to handle it in the calling function (e.g., post method)
